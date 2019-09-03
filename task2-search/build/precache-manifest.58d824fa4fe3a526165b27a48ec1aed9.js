2d6610a9184ccfc3d9dd4349de9665cf80e00378
Oleh Zharovskyi <olegzhora11@gmail.com>
2019-09-03 19:28:50 +0300

    add search with home page

---
+++ task2-search/build/precache-manifest.58d824fa4fe3a526165b27a48ec1aed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d6e4d9e56758f1bd17641105698cb0",
+    "url": "/React2-2/task2-search/build/index.html"
+  },
+  {
+    "revision": "46b3dcc34199f0e2abe1",
+    "url": "/React2-2/task2-search/build/static/css/main.c50de8c9.chunk.css"
+  },
+  {
+    "revision": "73165238d05c3d43621f",
+    "url": "/React2-2/task2-search/build/static/js/2.6dacd7dc.chunk.js"
+  },
+  {
+    "revision": "46b3dcc34199f0e2abe1",
+    "url": "/React2-2/task2-search/build/static/js/main.41023858.chunk.js"
+  },
+  {
+    "revision": "6a5861c0e0f592b8e8e8",
+    "url": "/React2-2/task2-search/build/static/js/runtime~main.d47659c9.js"
+  }
+]);
